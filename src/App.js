b1f84373a5ccc89b4d04a42a8b8a2ed01494db67
emmz3230 <emmz3230@gmail.com>
2022-02-16 09:13:26 -0800

    delete the written words and type my own

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React  from 'react';
 import './App.css';
 
+// go openweather to get the apikey 
+const api ={
+  key : '562054230351bd3897847fcd5e0dc379',
+  base: "http://api.openweathermap.org/data/2.5/"
+  
+} 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <main>
+        <div className='search-box'>
+          <input
+          type= "text"
+          className='search-bar'
+          placeholder='search...'
+          />
+        </div>
+        </main>
     </div>
   );
 }
